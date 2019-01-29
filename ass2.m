e537af9c6407cafd78a016ed923065ad8de5ac41
Vatsal Parmar <vparmar@sfu.ca>
2019-01-28 21:11:09 -0800

    Add files via upload

--- ass2.m
+++ ass2.m
@@ -14,20 +14,25 @@
 %               and paste this code into your own Matlab
 %               file. Follow the assignment sheet for further instructions to
 %               complete your report. 
+
 %  experimental parameters
 
 avgTimeDense = [];
 avgTimeUpper = [];
 avgTimePUpper = [];
-big = 5;
-small = 2;
+avgTimeDiag = [];
+avgTimeSparse = [];
 
+Nexmatrix = [100 3000 5000];
+
+Nmatrix = [200 300 400 500 600 700 800 900 1000 2000];
 
 for temp1 = 1:10
     
-N   = 1000*temp1;
-Nex = 75;
-
+N   = Nmatrix(temp1);
+NexD = Nexmatrix(1);
+NexU = Nexmatrix(3);
+NexPU = Nexmatrix(2);
 
 %  three matrix types
 
@@ -43,8 +48,8 @@ idx=randperm(N);
 Mp = Mt(idx,:); 
 
 %Tri-diagonal and sparse tri-diagonal
-%M3 = diag(diag(Md))+diag(diag(Md,-1),-1)+diag(diag(Md,1),1);
-%M3s=sparse(Mt)
+M3 = diag(diag(Md))+diag(diag(Md,-1),-1)+diag(diag(Md,1),1);
+M3s = sparse(Mt)
 
 %  exact solution of all ones
 x = ones(N,1);
@@ -54,46 +59,102 @@ bd = Md*x;
 bt = Mt*x;
 bp = bt(idx);
 
+ds = M3*x;
+dss = M3s*x;
+
 %  dense test
 tic 
-for jj = 1:(Nex*small*temp1)
+for jj = 1:NexD
     xd = Md\bd;
 end
 dense_time=toc;
 
-
 %  upper tri test
 tic
-for jj = 1:(Nex*250)
+for jj = 1:NexU
     xt = Mt\bt;
 end
 tri_time=toc;
 
-
 % permuted upper tri test  
 tic
-for jj = 1:(Nex*big*temp1)
+for jj = 1:NexPU
     xp = Mp\bp;
 end
 perm_tri_time=toc;
 
+%diagonal matrix
+tic
+for jj = 1:NexD
+    xds = M3\ds;
+end
+diagonal_time = toc;
+
+%sparse Matrix 
+tic
+for jj = 1:NexD
+    xdss = M3s\dss;
+end
+sparse_time = toc;
 
 %Computing avgerage solve times
-avg_tri_time = tri_time/(Nex);
+avg_tri_time = tri_time/(NexU);
 avgTimeUpper = [avgTimeUpper avg_tri_time];
 
-avg_perm_time = perm_tri_time/(Nex);
+avg_perm_time = perm_tri_time/(NexPU);
 avgTimePUpper = [avgTimePUpper avg_perm_time];
 
-avg_dense_time = dense_time/(Nex);
+avg_dense_time = dense_time/(NexD);
 avgTimeDense = [avgTimeDense avg_dense_time];
+
+avg_diag_time = diagonal_time/(NexD);
+avgTimeDiag = [avgTimeDiag avg_diag_time];
+
+avg_sparse_time = sparse_time/(NexD);
+avgTimeSparse = [avgTimeSparse avg_sparse_time];
+
 
 % You may find the following code helpful for displaying the results 
 % of this demo.
 type_times = {'Dense',avg_dense_time,'Upper Triangular', ...
               avg_tri_time,'permuted Upper Triangular', ...
-              avg_perm_time};
+              avg_perm_time, 'Diagonal', avg_diag_time, ...
+              'sparse', avg_sparse_time};
 fprintf(' \n')
 fprintf('Estimated time for a %s matrix is %f seconds. \n',type_times{:})
 
 end
+
+%getting the polyfit for every matrix types
+p1 = polyfit(log10(Nmatrix), log10(avgTimeDense), 1);
+p2 = polyfit(log10(Nmatrix), log10(avgTimeUpper), 1);
+p3 = polyfit(log10(Nmatrix), log10(avgTimePUpper), 1);
+p4 = polyfit(log10(Nmatrix), log10(avgTimeDiag), 1);
+p5 = polyfit(log10(Nmatrix), log10(avgTimeSparse), 1);
+
+figure(1); clf; hold on;
+grid on;
+xlabel('N matrix Size');
+ylabel('Time');
+
+y1fit = polyval(p1, log10(Nmatrix));
+plot(log10(Nmatrix), y1fit, 'r');
+
+y2fit = polyval(p2, log10(Nmatrix));
+plot(log10(Nmatrix), y2fit, 'b');
+
+y3fit = polyval(p3, log10(Nmatrix));
+plot(log10(Nmatrix), y3fit, 'g');
+
+y4fit = polyval(p4, log10(Nmatrix));
+plot(log10(Nmatrix), y4fit, 'y');
+
+y5fit = polyval(p5, log10(Nmatrix));
+plot(log10(Nmatrix), y5fit, 'c');
+
+plot(log10(Nmatrix), log10(avgTimeDense), 'ro');
+plot(log10(Nmatrix), log10(avgTimeUpper), 'bo');
+plot(log10(Nmatrix), log10(avgTimePUpper), 'go');
+plot(log10(Nmatrix), log10(avgTimeDiag), 'yo');
+plot(log10(Nmatrix), log10(avgTimeSparse), 'co');
+
